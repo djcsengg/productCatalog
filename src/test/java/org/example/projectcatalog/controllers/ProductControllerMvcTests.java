478c463a81c6aa4d6f5436be130776555ecf4b89
djcsengg <debashish.dj@dossiefoyer.in>
2026-03-17 13:01:25 +0530

    Added MVC test cases and tested

---
+++ src/test/java/org/example/projectcatalog/controllers/ProductControllerMvcTests.java
@@ -0,0 +1,94 @@
+package org.example.projectcatalog.controllers;
+
+
+import org.example.projectcatalog.controllers.ProductController;
+import org.example.projectcatalog.dtos.ProductDto;
+import org.example.projectcatalog.models.Product;
+import org.example.projectcatalog.services.IProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest(ProductController.class)
+public class ProductControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    @Qualifier("storageProductService")
+    private IProductService productService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void Test_GetProducts_RunSuccessfully() throws Exception {
+        //Arrange
+        Product product = new Product();
+        product.setId(1L);
+        product.setProductName("Iphone");
+
+        Product product2 = new Product();
+        product2.setId(2L);
+        product2.setProductName("Macbook");
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+        products.add(product2);
+        when(productService.getAllProducts()).thenReturn(products);
+
+
+        ProductDto productDto = new ProductDto();
+        productDto.setName("Iphone");
+        productDto.setId(1L);
+        ProductDto productDto2= new ProductDto();
+        productDto2.setId(2L);
+        productDto2.setName("Macbook");
+        List<ProductDto> productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+        productDtos.add(productDto2);
+
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(productDtos)));
+    }
+
+    @Test
+    public void Test_CreateProduct_RunSuccessfully() throws Exception {
+        //Arrange
+        ProductDto productDto = new ProductDto();
+        productDto.setId(10L);
+        productDto.setPrice(100000D);
+        productDto.setName("SuperComputer");
+
+        Product product = new Product();
+        product.setId(10L);
+        product.setProductPrice(100000D);
+        product.setProductName("SuperComputer");
+        when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+        //Act and Assert
+        mockMvc.perform(post("/products")
+                        .content(objectMapper.writeValueAsString(productDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(productDto)));
+    }
+}
